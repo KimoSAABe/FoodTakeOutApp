7191bb0adede295e50279757ad4a230964cf88b9
TMayekar <tmayekar@hostanalytics.com>
2021-04-14 15:18:36 +0530

    First Commit - Restaurants List and Restaurant Details-Menu Page Created

---
+++ lib/views/mobile/takeout/offers/offer_banner_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:swiggy_ui/widgets/responsive.dart';
+
+class OfferBannerView extends StatelessWidget {
+  final List<String> images = [
+    'assets/images/banner1.jpg',
+    'assets/images/banner2.jpg',
+    'assets/images/banner3.jpg',
+    'assets/images/banner4.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final isTabletDesktop = Responsive.isTabletDesktop(context);
+
+    return InkWell(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 15.0),
+        height: isTabletDesktop ? 260.0 : 180.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(isTabletDesktop ? 13.0 : 10.0),
+        ),
+        child: Swiper(
+          itemHeight: 100,
+          duration: 500,
+          itemWidth: double.infinity,
+          pagination: SwiperPagination(),
+          itemCount: images.length,
+          itemBuilder: (BuildContext context, int index) => Image.asset(
+            images[index],
+            fit: BoxFit.cover,
+          ),
+          autoplay: true,
+          viewportFraction: 1.0,
+          scale: 0.9,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+}
