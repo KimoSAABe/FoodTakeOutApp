58cee8ba99538eaff5f3d01180815d6e4edfeccc
Shruti Katpara <45987734+ShrutiKatpara@users.noreply.github.com>
2021-04-17 22:59:00 +0530

    added login screen page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:swiggy_ui/views/mobile/login/login_screen.dart';
 import 'shared/app_theme.dart';
-import 'views/mobile/mobile_screen.dart';
 import 'widgets/responsive.dart';
 
 void main() {
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: appPrimaryTheme(),
       home: Responsive(
-        mobile: MobileScreen(),
+        mobile: LoginPage(),
+        // mobile: MobileScreen(),
       ),
     );
   }
